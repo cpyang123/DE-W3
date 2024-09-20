60c5369b0db1b4b2e674eb608e1890d72ae32f7f
Peter Yang <cpyang@umich.edu>
2024-09-20 14:38:58 -0400

    transleted to polars

--- main.py
+++ main.py
@@ -1,28 +1,30 @@
-import pandas as pd
+import polars as pl
 import matplotlib.pyplot as plt
 
+# Read the CSV 
+sample_df = pl.read_csv("train.csv")
 
-sample_df = pd.read_csv("train.csv")
+# Calculate summary 
 summary = sample_df.describe()
 
+# check first couple of rows
+print(sample_df.head())
 
-# Refactor this into Functions
+# Drop the 'id' and 'Population' columns
+df_without_id = sample_df.drop(["id", "Population"])
 
-sample_df.head()
+columns = df_without_id.columns
+data = [df_without_id[column].to_list() for column in columns]
 
-# Drop the 'ID' column before plotting
-df_without_id = sample_df.drop(columns=["id", "Population"])
-
-# Create a box plot for all columns in the dataframe excluding 'ID'
 plt.figure(figsize=(8, 6))
-df_without_id.boxplot()
+plt.boxplot(data, tick_labels=columns)
 plt.title("Box and Whisker Plot (Excluding ID)")
 plt.ylabel("Value")
 
+# Save the box plot
 plt.savefig("fig/sample2.png")
 
-
-# Create a figure and axis
+# Create a figure and axis for the summary table
 fig, ax = plt.subplots(figsize=(10, 4))  # Adjust the size as necessary
 
 # Hide the axes
@@ -30,17 +32,22 @@ ax.xaxis.set_visible(False)
 ax.yaxis.set_visible(False)
 ax.set_frame_on(False)
 
+summary_data = summary.to_numpy().tolist() 
+column_labels = summary.columns
+row_labels = summary.get_column(
+    "statistic"
+).to_list()  
+
 # Create the table
 table = ax.table(
-    cellText=summary.values,
-    colLabels=summary.columns,
-    rowLabels=summary.index,
+    cellText=summary_data,
+    colLabels=column_labels,
+    rowLabels=row_labels,
     cellLoc="center",
     loc="center",
 )
 
-# Adjust the table scale
 table.scale(1, 2)
 
-# Show the plot with the table
+# Save the plot with the table
 plt.savefig("fig/summary.png")
--- test_main.py
+++ test_main.py
@@ -1,17 +1,25 @@
 from main import sample_df, summary
 import unittest
+import polars as pl
 import os
 
 
 class Test_Main(unittest.TestCase):
-    def test(self):
+    def test_sample_df(self):
+        # Check if the sample_df is not None
         self.assertTrue(sample_df is not None)
-        self.assertEqual(summary["id"]["count"], 37137)
+        # Verify the count of the 'id' column in the summary
+        self.assertEqual(
+            summary.filter(pl.col("statistic") == "count").get_column("id").item(),
+            37137,
+        )
 
     def test_summary(self):
+        # Check if the summary is not None
         self.assertTrue(summary is not None)
 
     def test_check_files(self):
+        # Check if the required files are present in the "fig/" directory
         self.assertTrue("sample.png" in os.listdir("fig/"))
         self.assertTrue("sample2.png" in os.listdir("fig/"))
         self.assertTrue("summary.png" in os.listdir("fig/"))
